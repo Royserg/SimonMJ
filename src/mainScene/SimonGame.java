3d97a9662782b28381c4cb6ef53908644bc163f9
Royserg <staniojakub@gmail.com>
2018-10-08 00:33:07 +0200

    Create Thread that plays sound and animation
    Previously, with normal loop, sounds and animations were all played
    at the same time, implementing Thread.sleep didn't help. Somehow it
    stopped activating animations.
    
    Creating a new Thread which activates animation and plays sound helped.
    
    Moreover, added few setters and getters

--- src/mainScene/SimonGame.java
+++ src/mainScene/SimonGame.java
@@ -1,18 +1,36 @@
 package mainScene;
 
+import animatefx.animation.BounceIn;
+
+
+import animatefx.animation.BounceInRight;
+import animatefx.animation.Flip;
+import animatefx.animation.Hinge;
+import javafx.animation.*;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
 
 import java.util.ArrayList;
 
 public class SimonGame {
 
-    @FXML
-    private Button btn_0;
-    private Button btn_1;
-    private Button btn_2;
-    private Button btn_3;
-
+    private Node btnNode;
 
     // make audioClips for every sound
     private SoundButton soundBtn1 = new SoundButton("btn_1", "btn_1.mp3");
@@ -28,25 +46,46 @@ public class SimonGame {
 
     private ArrayList<Integer> soundSequence = new ArrayList<>();
 
+    public SimonGame() {
+    }
 
+    /* === setters === */
     public void setLevel(int level) {
         gameLevel = level;
+    }
+
+//    public void setStage(Stage stage) {
+//        this.stage = stage;
+//    }
+
+    public void setBtnNode(Node btnNode){
+//        new BounceIn(btnNode).play();
     }
 
     public int levelUp() {
         return ++gameLevel;
     }
 
+    /* === getters === */
     int getLevel() {
         return gameLevel;
     }
 
-    void startGame() {
+
+
+    /* === Methods === */
+    void startGame(Button[] btnNodes) {
         // get random sound
         ChooseRandomSound();
-        ChooseRandomSound();
+//        ChooseRandomSound();
+
+        // works here
+//        new Hinge(btnNodes[2]).play();
+//        new BounceInRight(btnNodes[0]).play();
+//        new Flip(btnNodes[1]).play();
+
         // play sequence of sounds
-        playSoundSequence();
+        playSoundSequence(btnNodes);
 
         // wait for user to choose correct sounds
         // --- how to implement waiting ---
@@ -65,42 +104,92 @@ public class SimonGame {
         soundSequence.add(random);
     }
 
-    private void playSoundSequence() {
-        // play each song saved to the sequence
-        for (int soundNumber : soundSequence) {
-            // TODO animating the button
+    final DoubleProperty redColor = new SimpleDoubleProperty();
+    final DoubleProperty greenColor = new SimpleDoubleProperty();
+    final DoubleProperty blueColor = new SimpleDoubleProperty();
 
-            sounds[soundNumber].play();
 
-            // wait until playing next sound
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+
+
+    private void playSoundSequence(Button[] btnNodes) {
+
+        // TODO create Thread and runnable
+        Runnable runnable = () -> {
+            for (int soundNumber : soundSequence) {
+                new BounceIn(btnNodes[soundNumber]).play();
+                sounds[soundNumber].play();
+
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        }
 
-        System.out.println("sequence " + soundSequence);
+
+        };
+
+        Thread thread = new Thread(runnable);
+
+
+        thread.start();
+
+        // play each song saved to the sequence
+//        for (int soundNumber : soundSequence) {
+//            BounceIn animation = new BounceIn(btnNodes[soundNumber]);
+//            animation.play();
+//            sounds[soundNumber].play();
+
+//        }
+//            // TODO animating the button
+//
+//
+//            System.out.println("Node: " + btnNodes[soundNumber]);
+//
+//
+//
+//            /* ==== */
+//            sounds[soundNumber].play();
+//
+//            // wait until playing next sound
+//            try {
+//                Thread.sleep(delay);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+//        System.out.println("sequence " + soundSequence);
     }
 
-    void pressedSoundButton(String btn_id) {
+    void pressedSoundButton(Node btnNode) {
+        String btn_id = btnNode.getId();
         int soundNum = Character.getNumericValue(btn_id.charAt(btn_id.length() - 1));
         System.out.println("btn index: " + soundNum);
 
+        // animate button
+        new BounceIn(btnNode).play();
+
+        // play sound
+        sounds[soundNum].play();
+
         // check if is matching position in sequence
-        if (soundNum == soundSequence.get(userSequenceCounter)) {
-            System.out.println("sounds the same");
-
-            // play sound
-            sounds[soundNum].play();
-
-            btn_0.applyCss();
-
-            userSequenceCounter++;
-        } else {
-            // guessed wrong
-            System.out.println("Game Over");
-        }
+// below line works, just improve it a little TODO
+//        if (soundNum == soundSequence.get(userSequenceCounter)) {
+//            System.out.println("sounds the same");
+//
+//            // play sound
+//            sounds[soundNum].play();
+//
+//            userSequenceCounter++;
+//        } else {
+//            // guessed wrong
+//            System.out.println("Game Over");
+//        }
+//
+//        if (userSequenceCounter >= soundSequence.size()) {
+//            userSequenceCounter = 0;
+//        }
     }
 
 }
